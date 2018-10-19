8fc742851a2cbae93766daa9d6d1ac16429cacf4
Victoria Oh <ohvictori@gmail.com>
2018-10-19 15:32:17 -0700

    Add some UI logic for obtaining doctor name search.

---
+++ src/doctorSearch.js
@@ -0,0 +1,40 @@
+const Promise = require('es6-promise').Promise;
+
+export class DoctorSearch {
+
+  getDoctors(name) {
+    return new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&location=wa-seattle&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
+
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      }
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
+
+  // getSymptom() {
+  //   return new Promise(function(resolve, reject) {
+  //     let request = new XMLHttpRequest();
+  //     let url = ``;
+  //
+  //     request.onload = function() {
+  //       if (this.status === 200) {
+  //         resolve(request.response);
+  //       } else {
+  //         reject(Error(request.statusText));
+  //       }
+  //     }
+  //     request.open("GET", url, true);
+  //     request.send();
+  //   });
+  // }
+
+
+}
--- src/main.js
+++ src/main.js
@@ -2,8 +2,34 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
+import { DoctorSearch } from './doctorSearch.js';
 // const Promise = require('es6-promise').Promise;
 
 $(document).ready(function() {
+  $('#formDoctorName').submit(function() {
+    event.preventDefault();
+
+    let name = $('#doctorName').val();
+    console.log(name);
+    $('#doctorName').val("");
+
+    let doctorSearch = new DoctorSearch();
+    let promise = doctorSearch.getDoctors(name);
+
+    promise.then(function(response) {
+      let body = JSON.parse(response);
+
+      if(body.meta.count() === 0)
+
+      for(var i=0; i < body.data.length; i++) {
+        let name = body.data[i].first_name;
+        $("#resultDoctorName").text("doctorName");
+      }
+
+
+    }, function(error) {
+      $('.showErrors').text(`There was an error processing your request: ${error.message}`);
+    });
+  });
 
 });
